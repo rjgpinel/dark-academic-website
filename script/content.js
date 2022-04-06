2af75cf34dec42d88ceaa5af96cdef9f46a902c4
Thomas Chabal <t.chabal@hotmail.fr>
2022-04-07 00:06:45 +0200

    Moved code to content.js and added fields

---
+++ script/content.js
@@ -0,0 +1,324 @@
+// ===== START CUSTOM DATA =====
+const DATA = {
+    STYLE: {
+        NB_MAX_CONTRIBUTORS_PER_LINE: 3, // Integer in [1, 2, 3, 4, 6]
+        CONTRIBUTOR_IMAGE_SIZE: "180px",
+        FOOTER_LOGO_SIZE: "240px",
+    },
+    HEAD: {
+        FAVICON_SRC: "img/logo_dummy.png",
+        PROJECT_TITLE: "Project Title",
+        PROJECT_SUBTITLE: {
+            "name": "Conference of XXX 20XX",
+            "url": "#",
+        },
+        AUTHOR_NAME: "Ricardo Garcia",
+        VIDEO_SRC: "video/dummy_video.mp4",
+    },
+    ABSTRACT: {
+        TEXT: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra finibus quam, nec ornare mi posuere id. Nullam vitae efficitur neque. Maecenas rutrum nunc sit amet rhoncus iaculis. Pellentesque rutrum at nisl vitae dapibus. Maecenas auctor faucibus cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum a leo porttitor, mollis diam id, porta odio. Donec convallis porttitor lectus sed fringilla. Nulla facilisi. Curabitur tincidunt turpis sit amet leo lobortis lacinia.",
+            "Nullam rutrum leo quis purus cursus fermentum. Vivamus nec iaculis mi. Suspendisse pharetra eleifend turpis ut dictum. Vivamus nisi tellus, volutpat nec quam sit amet, ornare facilisis est. Fusce semper ultrices aliquet. Morbi sagittis eros ac dui rhoncus, sit amet semper purus varius. Duis finibus turpis orci, eu auctor nunc maximus nec. Curabitur aliquet tincidunt mauris faucibus iaculis. Donec sem orci, placerat quis tempor id, fringilla eu dolor. Etiam ultrices maximus lacus, et suscipit erat suscipit a. Proin est erat, maximus venenatis vestibulum vel, sagittis ac nisl. Proin fermentum ullamcorper tortor, eu rutrum libero mattis eu.",
+            "Pellentesque gravida augue augue, ac posuere lacus accumsan in. Suspendisse vel magna accumsan, porta lorem sed, eleifend massa. Proin auctor euismod ornare. Fusce convallis volutpat magna non laoreet. Quisque nibh nunc, tempus ac lacinia vel, condimentum sed est. Maecenas ultrices nunc eget est interdum semper. Nulla eu varius mi, non euismod enim. Mauris mollis urna ut diam fringilla rhoncus. Nam efficitur sem vel ultrices tempor. Vestibulum hendrerit, nulla vitae tempus ultricies, magna nulla pulvinar dui, eget accumsan ipsum arcu et lacus. Integer vel fringilla tellus. Donec pellentesque blandit elit a luctus."
+        ],
+        OVERVIEW: {
+            "src": "img/overview_dummy.png",
+            "legend": "Dummy overview of our method.",
+        }
+    },
+    CONTENT: [ // If you want to add other sections, add them with their title (used for navigation) and the html code of the section
+        {
+            "name": "Section 1",
+            "html": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra finibus quam, nec ornare mi posuere id. Nullam vitae efficitur neque. Maecenas rutrum nunc sit amet rhoncus iaculis. Pellentesque rutrum at nisl vitae dapibus. Maecenas auctor faucibus cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum a leo porttitor, mollis diam id, porta odio. Donec convallis porttitor lectus sed fringilla. Nulla facilisi. Curabitur tincidunt turpis sit amet leo lobortis lacinia.",
+        },
+        {
+            "name": "Section 2",
+            "html": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra finibus quam, nec ornare mi posuere id. Nullam vitae efficitur neque. Maecenas rutrum nunc sit amet rhoncus iaculis. Pellentesque rutrum at nisl vitae dapibus. Maecenas auctor faucibus cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum a leo porttitor, mollis diam id, porta odio. Donec convallis porttitor lectus sed fringilla. Nulla facilisi. Curabitur tincidunt turpis sit amet leo lobortis lacinia.",
+        },
+    ],
+    CONTRIBUTORS: [
+        {
+            "name": "Contributor 1",
+            "img": "img/contributor_dummy.png",
+            "url": "#"
+        },
+        {
+            "name": "Contributor 2",
+            "img": "img/contributor_dummy.png",
+            "url": "#"
+        },
+        {
+            "name": "Contributor 3",
+            "img": "img/contributor_dummy.png",
+            "url": "#"
+        },
+        {
+            "name": "Contributor 4",
+            "img": "img/contributor_dummy.png",
+            "url": "#"
+        },
+        {
+            "name": "Contributor 5",
+            "img": "img/contributor_dummy.png",
+            "url": "#"
+        },
+    ],
+    LINKS: [
+        {
+            "title": "Paper",
+            "icon": "img/paper.svg",
+            "links": [
+                {
+                    "name": "arXiv",
+                    "url": "#",
+                    "icon": "img/paper_copy.svg"
+                },
+            ],
+        },
+        {
+            "title": "Code",
+            "icon": "img/code.svg",
+            "links": [
+                {
+                    "name": "Repository 1",
+                    "url": "#",
+                    "icon": "img/GitHub-Mark-Light-32px.png"
+                },
+                {
+                    "name": "Repository 2",
+                    "url": "#",
+                    "icon": "img/GitHub-Mark-Light-32px.png"
+                },
+            ],
+        },
+    ],
+    CITATION: ` \
+        @article{articlename,
+            author    = {Author1 and Author2 and Author3 and Author4 and Author5},
+            title     = {Paper Title},
+            journal   = {arXiv preprint arXiv:TBD},
+            year      = {20XX}
+        }
+    `,
+    FOOTER: {
+        LOGOS: [
+            {
+                "name": "University 1",
+                "src": "img/logo_dummy.png",
+                "url": "#"
+            },
+            {
+                "name": "University 2",
+                "src": "img/logo_dummy.png",
+                "url": "#"
+            },
+        ],
+        COPYRIGHT: `Copyright Owner 20XX`
+    },
+}
+// ===== END CUSTOM DATA =====
+
+
+
+
+// /!\ --- Do not change following lines ---
+
+const NB_COLS = 12;
+const section_links_nav = DATA.CONTENT.map(section => ({ "id": idFromTitle(section), "name": section.name }));
+const NAVIGATION_LINKS = [{ "id": "abstract", "name": "Abstract" }].concat(
+    DATA.CONTENT.map(section => ({ "id": idFromTitle(section), "name": section.name })),
+    [
+        { "id": "contributors", "name": "Contributors" },
+        { "id": "links", "name": "Links" }
+    ]
+);
+
+
+/* Utils */
+function link(url, content, style = ``) {
+    return `<a href=${url} target="_blank" ${style}>${content}</a>`;
+}
+
+function padding(nbElements, nbElementsMax) {
+    const PADDING_COLS = (NB_COLS - nbElements * Math.floor(NB_COLS / nbElementsMax)) / 2;
+    return PADDING_COLS > 0 ? `<div class=col-lg-${PADDING_COLS}></div>` : ``;
+}
+
+function idFromTitle(title) {
+    return title.name.toLowerCase().replace(' ', '-');
+}
+
+/* Header */
+function displayTitle() {
+    let oneLineTitle = DATA.HEAD.PROJECT_TITLE;
+    const remove = ["<br>", "<br/>", "<br />"];
+    for (const val of remove) {
+        oneLineTitle = oneLineTitle.replace(val, " ");
+    }
+    document.head.querySelector("title").innerHTML = oneLineTitle;
+    document.head.querySelector("meta[name='author']").content = DATA.HEAD.AUTHOR_NAME;
+    document.head.querySelector("meta[name='description']").content = oneLineTitle;
+
+    document.head.querySelector("link[rel='icon']").type = `image/${DATA.HEAD.FAVICON_SRC.split(".").at(-1)}`;
+    document.head.querySelector("link[rel='icon']").href = DATA.HEAD.FAVICON_SRC;
+
+    document.getElementById("nav-project-title").innerHTML = oneLineTitle;
+    document.getElementById("head-title").innerHTML = DATA.HEAD.PROJECT_TITLE;
+    document.getElementById("head-subtitle").innerHTML = `<a href=${DATA.HEAD.PROJECT_SUBTITLE.url}>${DATA.HEAD.PROJECT_SUBTITLE.name}</a>`;
+}
+
+function displayNav() {
+    document.getElementById("navigation").innerHTML = NAVIGATION_LINKS.map(
+        link => `
+        <li class="nav-item">
+            <a class="nav-link active" href="#${link.id}">${link.name}</a>
+        </li>
+    `).join("");
+}
+
+/* Video section */
+function displayVideo() {
+    document.getElementById("video").innerHTML = `<iframe class="embed-responsive-item" src=${DATA.HEAD.VIDEO_SRC}></iframe>`;
+}
+
+/* Abstract section */
+function displayAbstract() {
+    document.getElementById("abstract-text").innerHTML = `
+    <div class="col-sm">
+        <h2 class="text-center">Abstract</h2>
+        <hr>
+        ${DATA.ABSTRACT.TEXT.map(text => `<p>${text}</p>`).join("")}
+        <br>
+        <br>
+    </div>
+  `;
+}
+
+function displayOverview() {
+    document.getElementById("overview").innerHTML = `
+        <div class="col-sm">
+            <img src=${DATA.ABSTRACT.OVERVIEW.src} alt="Method Overview" class="img-fluid">
+            <div id="overview-legend">${DATA.ABSTRACT.OVERVIEW.legend}</div>
+        </div>
+    `;
+}
+
+/* Content sections */
+function displayContentSections() {
+    document.getElementById("content").innerHTML = DATA.CONTENT.map(
+        section => `
+        <div class="container section" id=${idFromTitle(section)}>
+            <div class="row">
+                <div class="col-sm">
+                    <h2 class="text-center">${section.name}</h2>
+                    <hr>
+                    ${section.html}
+                </div>
+            </div>
+        </div>
+        `
+    ).join("");
+}
+
+/* Contributors section */
+function addPadding(nbContributorsInLine) {
+    return padding(nbContributorsInLine, DATA.STYLE.NB_MAX_CONTRIBUTORS_PER_LINE);
+}
+
+function displayContributorsRow(contributorsInRow) {
+    let htmlContent = `<div class="row text-center">` + addPadding(contributorsInRow.length);
+
+    for (const contributor of contributorsInRow) {
+        const htmlImg = `<img class="rounded-circle" src=${contributor.img} alt=${contributor.name} width=${DATA.STYLE.CONTRIBUTOR_IMAGE_SIZE} height=${DATA.STYLE.CONTRIBUTOR_IMAGE_SIZE} />`;
+        htmlContent +=
+            `<div class="col-lg-${Math.floor(NB_COLS / DATA.STYLE.NB_MAX_CONTRIBUTORS_PER_LINE)}">
+                ${link(contributor.url, htmlImg)}
+                <h4>${link(contributor.url, contributor.name)}</h4>
+            </div>`;
+    }
+    htmlContent += addPadding(contributorsInRow.length) + `</div>`;
+    return htmlContent;
+}
+
+function displayContributors() {
+    htmlContent = "";
+
+    nbLines = Math.ceil(DATA.CONTRIBUTORS.length / DATA.STYLE.NB_MAX_CONTRIBUTORS_PER_LINE)
+    for (const rowId of Array(nbLines).keys()) {
+        const contributorsInRow = DATA.CONTRIBUTORS.slice(rowId * DATA.STYLE.NB_MAX_CONTRIBUTORS_PER_LINE, (rowId + 1) * DATA.STYLE.NB_MAX_CONTRIBUTORS_PER_LINE);
+        htmlContent += displayContributorsRow(contributorsInRow);
+    }
+
+    document.getElementById("contributors").innerHTML += htmlContent;
+}
+
+
+/* Links section */
+function formatLink(_link) {
+    return `<div class="col-sm text-center">
+            ${link(_link.url, `<img src=${_link.icon} width="20px"> ${_link.name}`, `role="button" class="btn btn-dark"`)}
+        </div>`;
+}
+
+function formatLinkGroup(group) {
+    return `<div class="col-sm text-center">
+                <img src=${group.icon} />
+                <h4>${group.title}</h4>
+                ${group.links.map(link => formatLink(link)).join("")}
+            </div>`;
+}
+
+function displayLinks() {
+    document.getElementById("links-area").innerHTML = DATA.LINKS.map(
+        group => formatLinkGroup(group)
+    ).join("");
+}
+
+/* Citation */
+function displayCitation() {
+    htmlContent = `<code>${DATA.CITATION}</code>`;
+    document.getElementsByClassName("citation")[0].innerHTML = htmlContent;
+}
+
+/* Footer */
+function displayFooter() {
+    const nbLogos = DATA.FOOTER.LOGOS.length;
+    const nbColsPerDiv = Math.floor(NB_COLS / (nbLogos + 1));
+    const pad = padding(nbLogos + 1, nbLogos + 1);
+
+    const htmlLogos = DATA.FOOTER.LOGOS.map(logo => `\
+        <div class="col-md-${nbColsPerDiv} text-center">
+            ${link(logo.url, `<img src=${logo.src} alt=${logo.name} width=${DATA.STYLE.FOOTER_LOGO_SIZE}>`)}
+        </div>
+    `);
+
+    document.getElementById("footer").innerHTML = `\
+        <div class="container footer-container">
+            <div class="row align-items-center h-100">
+                ${pad}
+                ${htmlLogos.join("")}
+                <div class="col-md-${nbColsPerDiv} text-center align-middle">
+                    <h5>&copy; ${DATA.FOOTER.COPYRIGHT}</h5>
+                    Website Design: ${link("https://github.com/rjgpinel/dark-academic-website", "dark-academic-website")}
+                </div>
+                ${pad}
+            </div>
+        </div>
+    `;
+}
+
+/* Main */
+function displayPage() {
+    displayTitle();
+    displayNav();
+    displayVideo();
+    displayAbstract();
+    displayOverview();
+    displayContentSections();
+    displayContributors();
+    displayLinks();
+    displayCitation();
+    displayFooter();
+}
+
+displayPage();
